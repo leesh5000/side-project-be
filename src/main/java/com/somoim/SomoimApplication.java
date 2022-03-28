f91a3c224e1e1c886565d1453707d01d92b8f595
anwjrrp33 <anwjrrp33@gmail.com>
2022-03-28 17:08:28 +0900

    Basic Setup Commit

---
+++ src/main/java/com/somoim/SomoimApplication.java
@@ -0,0 +1,16 @@
+package com.somoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={ DataSourceAutoConfiguration.class })
+public class SomoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomoimApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/somoim/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.somoim.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String API_NAME = "Somiom API";
+    private static final String API_VERSION = "0.0.1";
+    private static final String API_DESCRIPTION = "Somiom API";
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .forCodeGeneration(true)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .enable(true);
+    }
+
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .version(API_VERSION)
+                .description(API_DESCRIPTION)
+                .build();
+    }
+}
